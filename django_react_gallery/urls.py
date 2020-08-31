574abf8115f781b0badab01ea864fdf975c05fa3
nickpatti <59921028+nickpatti@users.noreply.github.com>
2020-08-31 17:43:59 +0100

    Update urls.py

--- django_react_gallery/urls.py
+++ django_react_gallery/urls.py
@@ -21,8 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
-    path('', include('users.urls')),
-    path('', include('home.urls')),
     path('', include('gallery.urls')),
 ]
 
